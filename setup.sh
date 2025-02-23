e029c538be962e15f84d14dfa7d4255c46b71e72
Hunter McKay <huntmckay@gmail.com>
2025-02-23 19:02:08 +0000

    quick review, quick opinions, barely tested, mixture of some my dotfiles, send it

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ BASHRC=${HOME}/.bashrc
 BASH_ALIAS=${HOME}/.bash_alias
 BASH_FUNCTIONS=${HOME}/.bash_functions
 
+# user
+read -p "What is your Git username?" GIT_USER
+read -p "What is your Git email? " GIT_EMAIL
+
 function mklinks() {
     ln -s ${PWD}/nvim ${CONFIG}
     ln -s ${PWD}/bash/bashrc ${BASHRC}
@@ -16,4 +20,21 @@ function mklinks() {
     ln -s ${PWD}/bash/bash_functions ${BASH_FUNCTIONS}
 }
 
+function setgitalias() {
+    git config --global user.name "$GIT_USER"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global alias.co checkout
+    git config --global alias.br branch
+    git config --global alias.ci commit
+    git config --global alias.st status
+    git config --global alias.l "log -n 2 --stat"
+    git config --global core.editor "vim"
+    git config --global merge.tool vimdiff
+    git config --global help.autocorrect 1
+    git config --global color.ui auto
+    git config --global pull.rebase true
+    git config --global core.ignorecase true
+}
+
 mklinks
+setgitalias
