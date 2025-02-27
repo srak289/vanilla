5129fda04e907c3744962e71b5f73a8778d7756b
Spencer Rak <spencer.rak@gmail.com>
2025-02-27 13:44:36 +0000

    Snapshot

--- nvim/lua/srak/plugmap.lua
+++ nvim/lua/srak/plugmap.lua
@@ -1,20 +1,25 @@
 -- Harpoon Commands
 local harpoon = require("harpoon")
+
+-- debugging
+vim.g.harpon_log_level = "trace"
+
+-- REQUIRED
 harpoon:setup()
+-- REQUIRED
 
 harpoon.ui = require("harpoon.ui")
 harpoon.mark = require("harpoon.mark")
 
--- remove <leader>_h_ : causes delay for <leader>h :bp
-vim.keymap.set("n", "<leader>hk", function()
+vim.keymap.set("n", "<leader>u", function()
   harpoon.ui.toggle_quick_menu()
 end
 )
-vim.keymap.set("n", "<leader>hh", function()
+vim.keymap.set("n", "<leader>o", function()
   harpoon.ui.nav_prev()
 end
 )
-vim.keymap.set("n", "<leader>hl", function()
+vim.keymap.set("n", "<leader>i", function()
   harpoon.ui.nav_next()
 end
 )
