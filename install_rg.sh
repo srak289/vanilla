a9facc72593b51589927ae52cd730d09f24ac20e
Spencer Rak <spencer.rak@gmail.com>
2025-03-07 13:01:18 -0500

    Installs rg

--- install_rg.sh
+++ install_rg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euE -o pipefail
+
 if [ ! $(which wget) ]; then
     echo "Please install wget"
     exit 1
@@ -7,5 +9,9 @@ fi
 wget https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep-14.1.1-x86_64-unknown-linux-musl.tar.gz -O rg.tar.gz
 
 RG_TOPDIR=$(tar tf rg.tar.gz | sed -n 1p)
+LOCAL_BIN="${HOME}/.local/bin"
 
-tar -xf rg.tar.gz --xform "s/${RG_TOPDIR}/${HOME}\/.local\/bin/' ${RD_TOPDIR}/rg
+# trailing slash in RG_TOPDIR from tar listing
+tar -xf rg.tar.gz --xform "s/${RG_TOPDIR}/" ${RG_TOPDIR}rg
+rm rg.tar.gz
+mv rg ${LOCAL_BIN}
