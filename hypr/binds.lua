31eecfc80dce93f141a6f8abfe0e012525c9965b
Spencer Rak <spencer.rak@hadronindustries.com>
2026-05-20 14:48:33 -0400

    Snapshot

--- hypr/binds.lua
+++ hypr/binds.lua
@@ -28,7 +28,8 @@ hl.bind(mainMod .. " + SHIFT + L", hl.dsp.window.move({ direction = "right" }))
 hl.bind(mainMod .. " + SHIFT + K",  hl.dsp.window.move({ direction = "up" }))
 hl.bind(mainMod .. " + SHIFT + J",  hl.dsp.window.move({ direction = "down" }))
 
-hl.bind(mainMod .. " + SHIFT + Q",  hl.dsp.window.kill())
+hl.bind(mainMod .. " + SHIFT + Q",  hl.dsp.window.close())
+hl.bind(mainMod .. " + CTRL + SHIFT + Q",  hl.dsp.window.kill())
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
@@ -36,11 +37,13 @@ for i = 1, 10 do
   local key = i % 10 -- 10 maps to key 0
   hl.bind(mainMod .. " + " .. key,       hl.dsp.focus({ workspace = i}))
   hl.bind(mainMod .. " + SHIFT + " .. key,   hl.dsp.window.move({ workspace = i }))
+  hl.bind(mainMod .. " + CTRL + SHIFT + " .. key,   hl.dsp.window.move({ workspace = i, follow = false}))
 end
 
 -- Example special workspace (scratchpad)
 hl.bind(mainMod .. " + MINUS",     hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + MINUS", hl.dsp.window.move({ workspace = "special:magic" }))
+hl.bind(mainMod .. " + CTRL + SHIFT + MINUS", hl.dsp.window.move({ workspace = "special:magic", follow = false }))
 
 -- should toggle the "magic" state if the window is in scratch or viceversa
 
@@ -51,6 +54,21 @@ hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e-1" }))
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
 hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
+
+local nvRender = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia"
+
+hl.bind(mainMod .. " + BACKSLASH", hl.dsp.exec_cmd(nvRender .. " qutebrowser"), {})
+hl.bind(mainMod .. " + SHIFT + BACKSLASH", hl.dsp.exec_cmd(nvRender .. " firefox"), {})
+
+function mToggle()
+  if hl.get_monitor("eDP-1") ~= nil then
+    hl.monitor({ output = "eDP-1", disabled = true })
+  else
+    hl.monitor({ output = "eDP-1", disabled = false })
+  end
+end
+
+hl.bind(mainMod .. " + SHIFT + M", mToggle)
 
 -- Laptop multimedia keys for volume and LCD brightness
 hl.bind("XF86AudioRaiseVolume",
@@ -67,6 +85,10 @@ hl.bind("XF86MonBrightnessDown",
   { locked = true, repeating = true }
 )
 
+hl.bind("Print",
+  hl.dsp.exec_cmd("flameshot gui"),
+  { locked = true }
+)
 -- Requires playerctl
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),     { locked = true })
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
